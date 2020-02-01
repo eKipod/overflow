36a202a795ea342cf4d95117837556215566e996
Avi Dgany <ahd_one@hotmail.com>
2020-02-01 19:15:56 +1100

    Handle invalid volume

--- overflow.test/CalculatorTests.cs
+++ overflow.test/CalculatorTests.cs
@@ -1,12 +1,13 @@
 using Xunit;
 using overflow;
+using System;
 
 namespace overflow.test
 {
     public class CalculatorTests
     {
         [Fact]
-        public void CreateCalculator()
+        public void Create()
         {
             var calc = new Calculator();
             Assert.NotNull(calc);
@@ -14,13 +15,23 @@ namespace overflow.test
 
         [Theory]
         [InlineData(0, 0)]
+        [InlineData(0.9, 0.9)]
         [InlineData(1, 1)]
-        public void CalculateTopGlass(uint inVolume, uint expectedVolume)
+        [InlineData(1.1, 1)]
+        public void TopGlass(decimal inVolume, decimal expectedVolume)
         {
             var calc = new Calculator();
             var result = calc.GetVolume(row: 0, index: 0, inVolume: inVolume);
             Assert.NotNull(result);
             Assert.Equal(expectedVolume, result.Volume);
+        }
+
+
+        [Fact]
+        public void NegativeVolumeIsInvalid()
+        {
+            var calc = new Calculator();
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.GetVolume(0, 0, -1));
         }
 
     }
--- overflow/Calculator.cs
+++ overflow/Calculator.cs
@@ -10,7 +10,9 @@ namespace overflow
 
         public CalculationResult GetVolume(uint row, uint index, decimal inVolume)
         {
-            return new CalculationResult(inVolume);
+            if (inVolume < 0) throw new ArgumentOutOfRangeException(nameof(inVolume), inVolume, "Value cannot be negative");
+
+            return new CalculationResult(Math.Min(1, inVolume));
         }
     }
 }
